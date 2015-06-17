580f6aa0f87a996d9dd8869d357023ddfbe7a8bd
David Bruha <d.bruha@virtis.cz>
2015-06-17 14:03:28 +0200

    Modifications of the server for Debian

--- manifests/server.pp
+++ manifests/server.pp
@@ -25,9 +25,12 @@ class nis::server (
     ) {
 
     include nis::client
+    include nis::params
     include rpcbind
 
-    package { ["ypserv","yp-tools"]: ensure => latest }
+    if ! defined(Package[$::nis::params::nis_srv_package]) {
+      package { $::nis::params::nis_srv_package: ensure => latest }
+    }
 
     if ($nicknames) {
         file { "/var/yp/nicknames":
@@ -36,8 +39,8 @@ class nis::server (
             group   => "root",
             mode    => 0644,
             source  => $nicknames,
-            require => Package[["ypserv","ypbind","yp-tools"]],
-            notify  => Service["ypserv"]
+            require => Package[$::nis::params::nis_srv_package],
+            notify  => Service[$::nis::params::nis_srv_service],
         }
     }
 
@@ -48,55 +51,94 @@ class nis::server (
             group   => "root",
             mode    => 0644,
             source  => $securenets,
-            require => Package[["ypserv","ypbind","yp-tools"]],
-            notify  => Service["ypserv"]
+            require => Package[$::nis::params::nis_srv_package],
+            notify  => Service[$::nis::params::nis_srv_service],
         }
     }
 
-    if ($hostallow) {
-        add_nis_host_allow{$hostallow: process => "portmap"}
-    }
+    case $::osfamily {
+      'Debian': {
+        if ($master) {
+          augeas{ 'default nis master':
+            context => '/files/etc/default/nis',
+            changes => [
+              'set NISSERVER master',
+            ],
+            require => Package[$::nis::params::nis_srv_package],
+          }
+        } else {
+          augeas{ 'default nis slave':
+            context => '/files/etc/default/nis',
+            changes => [
+              'set NISSERVER slave',
+            ],
+            require => Package[$::nis::params::nis_srv_package],
+          }
+        }
 
-    augeas{ "ypserv service" :
-        context => "/files/etc/services",
-        changes => [
-            "ins service-name after service-name[last()]",
-            "set service-name[last()] ypserv",
-            "set service-name[.='ypserv']/port 834",
-            "set service-name[.='ypserv']/protocol tcp",
-            "ins service-name after service-name[last()]",
-            "set service-name[last()] ypserv",
-            "set service-name[.='ypserv'][2]/port 834",
-            "set service-name[.='ypserv'][2]/protocol udp",
-        ],
-        onlyif => "match service-name[port='834'] size == 0",
-        require => Package[["ypserv","ypbind","yp-tools"]]
+        exec { 'yp-config':
+          command => "domainname $ypdomain && ypinit -s $ypmaster",
+          path    => [ '/bin', '/usr/bin', '/usr/lib64/yp', '/usr/lib/yp', '/usr/sbin' ],
+          unless  => "test -d /var/yp/$ypdomain",
+          require => Package[$::nis::params::nis_srv_package],
+          notify  => Service[$::nis::params::nis_srv_service],
     }
+      }
+      'RedHat': {
+        if ($hostallow) {
+          add_nis_host_allow{$hostallow: process => "portmap"}
+        }
 
-    augeas{ "ypxfrd service" :
-        context => "/files/etc/services",
-        changes => [
-            "ins service-name after service-name[last()]",
-            "set service-name[last()] ypxfrd",
-            "set service-name[.='ypxfrd']/port 835",
-            "set service-name[.='ypxfrd']/protocol tcp",
-            "ins service-name after /files/etc/services/service-name[last()]",
-            "set service-name[last()] ypxfrd",
-            "set service-name[.='ypxfrd'][2]/port 835",
-            "set service-name[.='ypxfrd'][2]/protocol udp",
-        ],  
-        onlyif => "match service-name[port='835'] size == 0",
-        require => Package[["ypserv","ypbind","yp-tools"]]
+        augeas{ "ypserv service" :
+          context => "/files/etc/services",
+          changes => [
+              "ins service-name after service-name[last()]",
+              "set service-name[last()] ypserv",
+              "set service-name[.='ypserv']/port 834",
+              "set service-name[.='ypserv']/protocol tcp",
+              "ins service-name after service-name[last()]",
+              "set service-name[last()] ypserv",
+              "set service-name[.='ypserv'][2]/port 834",
+              "set service-name[.='ypserv'][2]/protocol udp",
+          ],
+          onlyif => "match service-name[port='834'] size == 0",
+          require => Package[$::nis::params::nis_srv_package],
+        }
+
+        augeas{ "ypxfrd service" :
+          context => "/files/etc/services",
+          changes => [
+              "ins service-name after service-name[last()]",
+              "set service-name[last()] ypxfrd",
+              "set service-name[.='ypxfrd']/port 835",
+              "set service-name[.='ypxfrd']/protocol tcp",
+              "ins service-name after /files/etc/services/service-name[last()]",
+              "set service-name[last()] ypxfrd",
+              "set service-name[.='ypxfrd'][2]/port 835",
+              "set service-name[.='ypxfrd'][2]/protocol udp",
+          ],
+          onlyif => "match service-name[port='835'] size == 0",
+          require => Package[$::nis::params::nis_srv_package],
+        }
+
+        augeas{ "nis server network" :
+          context => "/files/etc/sysconfig/network",
+          changes => [
+              "set YPSERV_ARGS '\"-p 834\"'",
+              "set YPXFRD_ARGS '\"-p 835\"'",
+          ],
+          require => Package[$::nis::params::nis_srv_package],
+          notify  => Service[$::nis::params::nis_srv_service],
+        }
+
+    exec { 'yp-config':
+        command => "domainname $ypdomain && ypinit -s $ypmaster && authconfig --enablenis --enablekrb5 --kickstart",
+        path    => [ '/bin', '/usr/bin', '/usr/lib64/yp', '/usr/lib/yp', '/usr/sbin' ],
+        unless  => "test -d /var/yp/$ypdomain",
+        require => Package[$::nis::params::nis_srv_package],
+        notify  => Service[$::nis::params::nis_srv_service],
     }
-
-    augeas{ "nis server network" :
-        context => "/files/etc/sysconfig/network",
-        changes => [
-            "set YPSERV_ARGS '\"-p 834\"'",
-            "set YPXFRD_ARGS '\"-p 835\"'",
-        ],
-        notify => Service["ypserv"],
-        require => Package[["ypserv","ypbind","yp-tools"]]
+      }
     }
 
     augeas{ "nis server nicknames" :
@@ -107,22 +149,16 @@ class nis::server (
             "set hosts/map hosts.byname",
             "set netgroup/map netgroup",
         ],
-        notify => Service["ypserv"],
-        require => Package[["ypserv","ypbind","yp-tools"]]
+        require => Package[$::nis::params::nis_srv_package],
+        notify  => Service[$::nis::params::nis_srv_service],
     }
 
-    exec { "yp-config":
-        command => "domainname $ypdomain && ypinit -s $ypmaster && authconfig --enablenis --enablekrb5 --kickstart",
-        path => [ '/bin', '/usr/bin', '/usr/lib64/yp', '/usr/lib/yp', '/usr/sbin' ],
-        unless => "test -d /var/yp/$ypdomain",
-        notify  => [Service["ypserv"],Service["ypbind"]],
-        require => Package[["ypserv","ypbind","yp-tools"]]
-    }
-
-    service { "ypserv":
-              ensure => running,
-              enable => true,
-              hasrestart => true,
-              require => Package["ypserv"]
+    if ! defined(Service[$::nis::params::nis_srv_service]) {
+      service { $::nis::params::nis_srv_service:
+        ensure => running,
+        enable => true,
+        hasrestart => true,
+        require => Package[$::nis::params::nis_srv_package],
+      }
     }
 }
